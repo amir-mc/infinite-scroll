76057f127768723bb0a68e913109a1ebd389c6a5
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-10-26 12:43:06 +0330

    Add files via upload

---
+++ src/component/coment.jsx
@@ -0,0 +1,11 @@
+const Comennts = ({name,description})=>{
+    return(
+        <>
+            <p className="">{name}</p>
+    <hr></hr>
+    <p>{description}</p>
+        </>
+
+    )
+} 
+export default Comennts  
